e2c3d78a300ba696d7c232ba7a7eecdcf783efb0
martinzuro <martinzuro@gmail.com>
2023-10-17 15:47:26 -0300

    Add onWindowFocusChanged(p0: Boolean) method

--- android/src/main/kotlin/com/stripe/stripe_terminal/StripeTerminalPlugin.kt
+++ android/src/main/kotlin/com/stripe/stripe_terminal/StripeTerminalPlugin.kt
@@ -666,6 +666,10 @@ class StripeTerminalPlugin : FlutterPlugin, MethodCallHandler,
         TODO("Not yet implemented")
     }
 
+    override fun onWindowFocusChanged(p0: Boolean) {
+        TODO("Not yet implemented")
+    }
+
     override fun onConfigurationChanged(p0: Configuration) {
         TODO("Not yet implemented")
     }
